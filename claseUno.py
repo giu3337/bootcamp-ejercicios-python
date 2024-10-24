82a1129008be540978c6d5925ce00cdc120bcec7
Giuseppe Garcia-Cancela <giu3337@icloud.com>
2024-10-23 20:51:11 -0500

    nuevos datos python

--- claseUno.py
+++ claseUno.py
@@ -1,10 +1,38 @@
 #Clase Uno de la clase de Python
 
-
-# Variables
-
-
-
 # Terminal 
 # Aprender a usar la terminal de comandos desde el comando.
 # Aprender a usar Git. 
+
+
+# Variables
+# Variables en Python son objetos que guardan un valor.
+#type() -> Función que nos permite saber el tipo de dato de una variable.
+
+#Datos Primitivos
+
+nombre = "Giuseppe"
+numero = 34
+decimal = 3.14 #float
+booleano = True
+
+#Datos Compuestos
+
+#Listas
+lista = [1,2,3,4,5] #Array
+#Tuplas
+tupla = (1,2,3,4,5) #Array Inmutable
+#Diccionarios
+diccionario = {
+    "nombre": "Giuseppe",
+    "edad": 34,
+    "nacionalidad": "Argentino"
+} #Objeto
+
+
+#Strings
+
+
+
+
+
