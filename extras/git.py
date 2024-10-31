f95619a55fc73f4ffe7a9dfc0ecf688a606c81a1
Giuseppe Garcia-Cancela <giu3337@icloud.com>
2024-10-30 21:48:25 -0500

    add git.py

---
+++ extras/git.py
@@ -0,0 +1,10 @@
+# git
+
+# git init
+# git add . # or git add <file>
+# git commit -m "first commit"
+# git remote add origin
+# git push -u origin master
+# git pull origin master # git pull <remote> <branch> 
+# git clone <url>
+# git branch <branch>
